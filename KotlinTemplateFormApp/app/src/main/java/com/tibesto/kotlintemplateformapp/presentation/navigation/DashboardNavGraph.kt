7f15bd76c65f2e09641d9dc6729a79fe5883161d
sharansudheer <saran_sudheer@hotmail.com>
2025-10-25 19:41:49 +0530

    push:check-point

--- KotlinTemplateFormApp/app/src/main/java/com/tibesto/kotlintemplateformapp/presentation/navigation/DashboardNavGraph.kt
+++ KotlinTemplateFormApp/app/src/main/java/com/tibesto/kotlintemplateformapp/presentation/navigation/DashboardNavGraph.kt
@@ -7,22 +7,22 @@ import androidx.navigation.compose.composable
 import androidx.navigation.NavController
 import com.tibesto.kotlintemplateformapp.presentation.Dashboard.*
 
-fun NavGraphBuilder.dashboardNavGraph(navController: NavController) {
-    navigation(
-        startDestination = DashboardRoutes.Main.route,
-        route = "dashboard_graph"
-    ) {
-        composable(DashboardRoutes.Main.route) {
-            MainPatientDashboardScreen(navController)
-        }
-        composable(DashboardRoutes.Ledger.route) {
-            LedgerScreen(navController)
-        }
-        composable(DashboardRoutes.Appointments.route) {
-            AppointmentScreen(navController)
-        }
-        composable(DashboardRoutes.Summary.route) {
-            SummaryScreen(navController)
-        }
-    }
-}
+//fun NavGraphBuilder.dashboardNavGraph(navController: NavController) {
+//    navigation(
+//        startDestination = DashboardRoutes.Main.route,
+//        route = "dashboard_graph"
+//    ) {
+//        composable(DashboardRoutes.Main.route) {
+//            MainPatientDashboardScreen(navController)
+//        }
+//        composable(DashboardRoutes.Ledger.route) {
+//            LedgerScreen(navController)
+//        }
+//        composable(DashboardRoutes.Appointments.route) {
+//            AppointmentScreen(navController)
+//        }
+//        composable(DashboardRoutes.Summary.route) {
+//            SummaryScreen(navController)
+//        }
+//    }
+//}
